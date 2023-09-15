483dfbcf1f638df59b2fecac4c592a7878ebbc95
Majasun <144233788+Majasun@users.noreply.github.com>
2023-09-15 11:04:57 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,6 +1,20 @@
-# Write a program that determines whether a year entered by the user is a leap year or not using ifelif-else statements.
-year = int(input("Enter a year: "))
-if (year % 4 == 0 and year % 100 != 0)or year%400:
-  print(year, "is a leap year")
-else:
-  print(year, "is not a leap year")
+#
+class Player:
+    def play(self):
+        print("The player is playing cricket")
+
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting")
+
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling")
+
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# Call the play() method for each object
+batsman.play()
+bowler.play()
